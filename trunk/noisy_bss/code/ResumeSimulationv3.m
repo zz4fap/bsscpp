3a89b907e7743d449206ed1b6b476435daddc765
zz4fap <zz4fap@362d6d2c-4c02-bf13-b717-816be16ce2d1>
2012-04-24 10:07:16 +0000

    fix problem when resuming simulation and add code to script1

--- trunk/noisy_bss/code/ResumeSimulationv3.m
+++ trunk/noisy_bss/code/ResumeSimulationv3.m
@@ -5,10 +5,15 @@ filename = strcat(savingPath,filename);
 fid = fopen(filename, 'rb');
 
 if(fid < 0)
-    result = -1;
+    result = -1; nextN = -1; nextL = -1; nextStepsize = -1; recordingCounter = -1; NPos = -1; ssPos = -1;
 else
     while(1)
         aux = fscanf(fid,'%d %d', 2);
+        if(isequal(aux,[])==1)
+            result = -1; nextN = -1; nextL = -1; nextStepsize = -1; recordingCounter = -1; NPos = -1; ssPos = -1;
+            return;
+        end
+        
         if(feof(fid)~=1)
             number = aux;
         else
--- trunk/noisy_bss/code/script1.m
+++ trunk/noisy_bss/code/script1.m
@@ -2,7 +2,7 @@
 %                                   Tests started on xx_11_2011
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%------- Testing separation between aurora's data base noise and Ynoguti's data base speaker--------
+%------- Testing the separation between aurora's noise data base and Ynoguti's speaker data base --------
 
 % Script 1 -> 1 to 120
 % fs = 8000
@@ -13,4 +13,30 @@
 clear all;
 clc;
 
-ConfigurationScript('recordings_1');
+NumberOfEpochs = 500;
+
+fs = [8000]; % Sampling frequency is given in Hz
+
+noises = struct('noisefile',{'babble.wav'});
+speakers = GetSpeakersListFromFile('recordings_1');
+numberOfRecordings = length(speakers);
+
+filename = 'SimulationsInformationForRecordings_1.txt';
+equalizeEnergyToGreaterOne = true;
+
+% Location of the speech and noise files
+speakersPath = '/Users/zz4fap/Desktop/BSS+ASR/data_base/speech/todos/treino/wav/';
+noisesPath = '/Users/zz4fap/Desktop/BSS+ASR/data_base/noises/wav/short/';
+
+if(equalizeEnergyToGreaterOne)
+    savingPath = '/Users/zz4fap/Desktop/BSS+ASR/simulations/EqualizedEnergy/';
+else
+    savingPath = '/Users/zz4fap/Desktop/BSS+ASR/simulations/EnergyNotEqualized/';
+end
+
+stepsize = [0.002 0.004 0.006 0.008 0.01 0.03];
+
+N = [256 256 512 512];
+L = [64 128 128 256];
+
+StartAndOrResumeSimulations(fs, speakers, noises, filename, N, L, stepsize, NumberOfEpochs, equalizeEnergyToGreaterOne, numberOfRecordings, speakersPath, noisesPath, savingPath);
